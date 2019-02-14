f078ba8fbc8ab7b36b75bbd056af4bc4bc358a74
Zak <34078361+BoSkeet@users.noreply.github.com>
2019-02-13 20:24:25 -0800

    Update Helper.h
    File IO in constructor

--- CPP/CompilerCPP/Helper.h
+++ CPP/CompilerCPP/Helper.h
@@ -1,8 +1,27 @@
 #pragma once
 
 #include <string>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <list>
+
 namespace Helper
 {
+enum class TokenType
+{
+  Keyword,
+  Identifier,
+  Separator,
+  Operator
+};
+
+class NotImplementedException : public std::logic_error
+{
+public:
+    virtual char const * what() const { return "Function not yet implemented."; }
+} NotImplementedEx;
+
 class Helper
 {
 public:
@@ -12,14 +31,39 @@ public:
   bool Helper::IsIdentifier(std::string);
   bool Helper::IsSeparator(std::string);
   bool Helper::IsOperator(std::string);
+
+private:
+  std::list<std::string> PreParse;
+  std::string Token;
+  std::map<TokenType, std::string> PostParse;
 };
 
 Helper::Helper()
 {
 }
 
+//
+// Read from given file path or from default path and load into PreParse
+//
 Helper::Helper(std::string input)
 {
+  std::ifstream inputFile(input + "txt");
+  if (inputFile.is_open)
+  {
+    std::string lineParse;
+    int count = 0;
+    while (std::getline (inputFile,lineParse))
+    {
+      PreParse.assign(count,lineParse);
+      ++count;
+    }
+    inputFile.close;
+  }
+  else
+  {
+    throw NotImplementedEx;
+  }
+  
 }
 
 bool Helper::IsKeyword(std::string input)
@@ -52,7 +96,7 @@ bool Helper::IsSeparator(std::string input)
 
 bool Helper::IsOperator(std::string input)
 {
-  if(input==">"||input=="=")
+  if (input == ">" || input == "=")
   {
     return true;
   }
@@ -60,6 +104,5 @@ bool Helper::IsOperator(std::string input)
   {
     return false;
   }
-  
 }
 } // namespace Helper
