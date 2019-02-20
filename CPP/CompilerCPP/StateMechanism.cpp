302ed630b4d8fa1e040f5515ef26cf49d8530976
Zak Kara <34078361+BoSkeet@users.noreply.github.com>
2019-02-20 11:47:59 -0800

    Building out StateMechanism

--- CPP/CompilerCPP/StateMechanism.cpp
+++ CPP/CompilerCPP/StateMechanism.cpp
@@ -3,221 +3,348 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <regex>
 
 namespace StateMechanism
 {
-	enum TokenType
-	{
-		Keyword,
-		Identifier,
-		Separator,
-		Operator
-	};
+enum TokenType
+{
+	Keyword,
+	Identifier,
+	Separator,
+	Operator
+};
 
-	enum States
-	{
-		Process,
-		FetchOperation,
-		Stop
-	};
+enum States
+{
+	Process,
+	FetchOperation,
+	Stop
+};
 
-	class NotImplementedException : public std::logic_error
-	{
-	public:
-		virtual char const *what() const { return "Function not yet implemented."; }
-	} NotImplementedEx;
+class NotImplementedException : public std::logic_error
+{
+  public:
+	virtual char const *what() const { return "Function not yet implemented."; }
+} NotImplementedEx;
 
-	class Helper
-	{
-	public:
-		Helper();
-		Helper(std::string);
-		bool IsKeyword(std::string);
-		bool IsIdentifier(std::string);
-		bool IsSeparator(std::string);
-		bool IsSeparator(char);
-		bool IsOperator(char);
-		bool IsWhitespaceOrSeparatorOrOperator(char);
-		char GetNext();
-		void PushPreParse(char);
-		std::list<std::string> GetNext(int);
-		std::string Token;
-		std::map<TokenType, std::string> PostParse;
-	private:
-		std::list<char> PreParse;
-	};
+class Helper
+{
+  public:
+	Helper();
+	Helper(std::string);
+	bool IsKeyword(std::string);
+	bool IsIdentifier(std::string);
+	bool IsSeparator(std::string);
+	bool IsSeparator(char);
+	bool IsOperator(char);
+	bool IsNumber(std::string);
+	bool IsComment(char);
+	bool IsWhitespaceOrSeparatorOrOperator(char);
+	char GetNext();
+	void PushPreParse(char);
+	char PopPreParse();
+	bool IsPreParseEmpty();
+	std::list<std::string> GetNext(int);
+	std::string Token;
+	std::map<TokenType, std::string> PostParse;
 
-	Helper::Helper()
+  private:
+	std::list<char> PreParse;
+};
+
+Helper::Helper()
+{
+	std::ifstream inputFile;
+	inputFile.open("Input.txt", std::ios::in);
+	if (inputFile.is_open)
 	{
-		std::ifstream inputFile;
-		inputFile.open("Input.txt", std::ios::in);
-		if (inputFile.is_open)
+		char charParse;
+		int count = 0;
+		while (inputFile.get(charParse))
 		{
-			char charParse;
-			int count = 0;
-			while (inputFile.get(charParse))
-			{
-				PreParse.assign(count, charParse);
-				++count;
-			}
-			inputFile.close;
+			PreParse.assign(count, charParse);
+			++count;
 		}
-		else
-		{
-			throw NotImplementedEx;
-		}
+		inputFile.close;
 	}
-
-	//
-	// Read from given file path or from default path and load into PreParse
-	// fills chars into PreParse
-	//
-	Helper::Helper(std::string input)
-	{
-		std::ifstream inputFile;
-		inputFile.open(input + ".txt", std::ios::in);
-		if (inputFile.is_open)
-		{
-			char charParse;
-			int count = 0;
-			while (inputFile.get(charParse))
-			{
-				PreParse.assign(count, charParse);
-				++count;
-			}
-			inputFile.close;
-		}
-		else
-		{
-			throw NotImplementedEx;
-		}
-	}
-
-	bool Helper::IsKeyword(std::string input)
-	{
-		if (input == "int" || input == "if"
-			|| input == "else")
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	bool Helper::IsIdentifier(std::string input)
-	{
-	}
-
-	bool Helper::IsSeparator(std::string input)
-	{
-		if (input == "," || input == "("
-			|| input == ")" || input == ";"
-			|| input == "{" || input == "}")
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	bool Helper::IsSeparator(char input)
-	{
-		if (input == ',' || input == '('
-			|| input == ')' || input == ';'
-			|| input == '{' || input == '}')
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	bool Helper::IsOperator(char input)
-	{
-		if (input == '>' || input == '=')
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	bool Helper::IsWhitespaceOrSeparatorOrOperator(char input)
-	{
-		if (input == ' ' || IsSeparator(input)
-			|| IsOperator(input))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	// Untested
-	//
-	// Return next string from PreParse list
-	//
-	char Helper::GetNext()
-	{
-		char returnChar = PreParse.front;
-		PreParse.pop_front;
-		return returnChar;
-	}
-
-	// Untested
-	//
-	// Return list<string> with next multiple items in PreParse List
-	//
-	std::list<std::string> Helper::GetNext(int iterator)
-	{
-		std::list<std::string> returnList;
-		for (int i = 0; i < iterator; i++)
-		{
-			returnList.assign(PreParse.front);
-			PreParse.pop_front;
-		}
-	}
-
-	void Helper::PushPreParse(char input){
-		PreParse.push_front(input);
-	}
-
-	class StateMechanism
-	{
-		StateMechanism();
-		StateMechanism(Helper helper);
-		States StateIs(TokenType);
-		void StateIsKeyword();
-		void StateIsIdentifier();
-		void StateIsSeparator();
-		void StateIsOperator();
-		void StateIsNumber();
-		void StateIsComments();
-	};
-
-	StateMechanism::StateMechanism()
+	else
 	{
 		throw NotImplementedEx;
 	}
+}
 
-	// Initial State
-	StateMechanism::StateMechanism(Helper helper)
+//
+// Read from given file path or from default path and load into PreParse
+// fills chars into PreParse
+//
+Helper::Helper(std::string input)
+{
+	std::ifstream inputFile;
+	inputFile.open(input + ".txt", std::ios::in);
+	if (inputFile.is_open)
 	{
-		std::string strBuilder;
-		char charBuilder;
-		while(!helper.IsWhitespaceOrSeparatorOrOperator((charBuilder=helper.GetNext())))
+		char charParse;
+		int count = 0;
+		while (inputFile.get(charParse))
 		{
-			strBuilder+=charBuilder;
-		} 
+			PreParse.assign(count, charParse);
+			++count;
+		}
+		inputFile.close;
 	}
+	else
+	{
+		throw NotImplementedEx;
+	}
+}
+
+bool Helper::IsKeyword(std::string input)
+{
+	if (input == "int" || input == "if" || input == "else")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Helper::IsIdentifier(std::string input)
+{
+}
+
+bool Helper::IsSeparator(std::string input)
+{
+	if (input == "," || input == "(" || input == ")" || input == ";" || input == "{" || input == "}")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Helper::IsSeparator(char input)
+{
+	if (input == ',' || input == '(' || input == ')' || input == ';' || input == '{' || input == '}')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Helper::IsOperator(char input)
+{
+	if (input == '>' || input == '=')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// Untested
+bool Helper::IsNumber(std::string input)
+{
+	std::regex r("[0-9]+:[0-9]+");
+	if (std::regex_match(input, r))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Helper::IsComment(char input)
+{
+	if (input == '!')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Helper::IsWhitespaceOrSeparatorOrOperator(char input)
+{
+	if (input == ' ' || IsSeparator(input) || IsOperator(input))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// Untested
+//
+// Return next string from PreParse list
+//
+char Helper::GetNext()
+{
+	char returnChar = PreParse.front;
+	PreParse.pop_front;
+	return returnChar;
+}
+
+// Untested
+//
+// Return list<string> with next multiple items in PreParse List
+//
+std::list<std::string> Helper::GetNext(int iterator)
+{
+	std::list<std::string> returnList;
+	for (int i = 0; i < iterator; i++)
+	{
+		returnList.assign(PreParse.front);
+		PreParse.pop_front;
+	}
+}
+
+void Helper::PushPreParse(char input)
+{
+	PreParse.push_front(input);
+}
+
+char Helper::PopPreParse()
+{
+	char returnChar = PreParse.front;
+	PreParse.pop_front;
+	return returnChar;
+}
+
+bool Helper::IsPreParseEmpty()
+{
+	if (PreParse.empty())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+class StateMechanism
+{
+	StateMechanism();
+	StateMechanism(std::string);
+	void StateIs(Helper);
+	void StateIsKeyword(std::string);
+	void StateIsIdentifier(std::string);
+	void StateIsSeparator(char);
+	void StateIsOperator(char);
+	void StateIsNumber(std::string);
+	void StateIsComment(char);
+	void StateIsWhiteSpace();
+	States CurrentState;
+};
+
+// Pre-Initial State
+StateMechanism::StateMechanism()
+{
+	Helper helper;
+	StateIs(helper);
+}
+
+// Pre-Initial State
+StateMechanism::StateMechanism(std::string fileName)
+{
+	Helper helper(fileName);
+	StateIs(helper);
+}
+
+// Initial State
+void StateMechanism::StateIs(Helper helper)
+{
+	while (!helper.IsPreParseEmpty())
+	{
+		char charBuilder;
+		std::string strBuilder;
+		while (!helper.IsWhitespaceOrSeparatorOrOperator(charBuilder = helper.PopPreParse()))
+		{
+			strBuilder += charBuilder;
+		}
+
+		// Test strBuilder
+		if (helper.IsKeyword(strBuilder))
+		{
+			StateIsKeyword(strBuilder);
+		}
+		else if (helper.IsNumber(strBuilder))
+		{
+			StateIsNumber(strBuilder);
+		}
+		else
+		{
+			StateIsIdentifier(strBuilder);
+		}
+
+		// Test charBuilder
+		if (helper.IsSeparator(charBuilder))
+		{
+			StateIsSeparator(charBuilder);
+		}
+		else if (helper.IsOperator(charBuilder))
+		{
+			StateIsOperator(charBuilder);
+		}
+		else if (helper.IsComment(charBuilder))
+		{
+			StateIsComment(charBuilder);
+		}
+		else
+		{
+			StateIsWhiteSpace();
+		}
+	}
+}
+
+void StateMechanism::StateIsKeyword(std::string input)
+{
+	throw NotImplementedEx;
+}
+
+void StateMechanism::StateIsIdentifier(std::string input)
+{
+	throw NotImplementedEx;
+}
+
+void StateMechanism::StateIsSeparator(char input)
+{
+	throw NotImplementedEx;
+}
+
+void StateMechanism::StateIsOperator(char input)
+{
+	throw NotImplementedEx;
+}
+
+void StateMechanism::StateIsNumber(std::string input)
+{
+	throw NotImplementedEx;
+}
+
+void StateMechanism::StateIsComment(char input)
+{
+	throw NotImplementedEx;
+}
+
+void StateMechanism::StateIsWhiteSpace()
+{
+	throw NotImplementedEx;
+}
 } // namespace StateMechanism
